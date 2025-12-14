4cb5af6205b4676666e9478c397c7e937986fdb1
MuhammadAlvi03 <muhammadalvics@gmail.com>
2025-12-14 15:42:27 -0600

    initial commit

---
+++ Cell.java
@@ -0,0 +1,11 @@
+
+public class Cell {
+    public int row;
+    public int col;
+    public boolean visited = false;
+    
+    public Cell(int r, int c) {
+        row = r;
+        col = c;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,11 @@
+/*
+Modules needed:
+
+*/
+
+
+public class Main {
+    public static void main(String args[]) {
+
+    }
+}
