4a1ea8820be4b5f79a262bf1adfa3f1e1efe61e5
MuhammadAlvi03 <muhammadalvics@gmail.com>
2025-12-14 20:36:26 -0600

    create main function

--- Main.java
+++ Main.java
@@ -1,11 +1,19 @@
-/*
-Modules needed:
-
-*/
-
-
+import java.util.List;
+import java.util.Scanner;
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter maze dimensions: ");
+        int rows = input.nextInt();
+        int cols = input.nextInt();
+        Maze maze = new Maze(rows, cols);
+        maze.generateMaze();
 
+        System.out.println("Generated Maze:");
+        Display.printMaze(maze);
+
+        List<Integer> path = MazeSolver.solveMaze(maze);
+        System.out.println("\nMaze Solver:");
+        Display.printSolvedMaze(maze, path);
     }
 }
